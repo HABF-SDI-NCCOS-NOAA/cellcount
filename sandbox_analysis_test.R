6ffd5a856eff5b9db330aeea80088547c5a033bc
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-04-19 08:57:57 -0400

    Updated file - replaced old sandbox script

--- sandbox_analysis_test.R
+++ sandbox_analysis_test.R
@@ -6,19 +6,24 @@ library(beepr)
 library(outliers)
 library(cellcount)
 
+mapped_avg <- function(x) {
+  x <- as.matrix(x)
+  return(x)
+}
+
 # clear environment and free unused memory
 gc()
 
 Cell.Count <- data.frame(Image_File_Name = character(0), Cell_Count = numeric(0))
 
-file_name<-paste0("Dolichospermum_") #Change the name of species analyzed here
+file_name<-paste0("F192_") #Change the name of species analyzed here
 
 #Change directories here
 savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/CSV_data/")
-image_savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/Convert_Images/Cyano F271_40x/Med/Rep 1/")
-images <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/F271_40x/Med/Rep 1/"
+image_savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/Convert_Images/Cyano F192_40x/Rep 1/Conc_E/")
+images <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/Cyano F192_40x/Rep 1/Conc_E/"
                      , pattern = "tif", full.name = T)
-images_names <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/F271_40x/Med/Rep 1/"
+images_names <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/Cyano F192_40x/Rep 1/Conc_E/"
                            , pattern = "tif", full.name = F)
 
 imgNames <- paste0(file_name, images_names)
@@ -26,32 +31,20 @@ read_images <- lapply(images, readTIFF)
 img_transposed <- lapply(read_images,aperm,c(2,1,3))
 names(images) <- imgNames
 
-grey_images <- lapply(img_transposed, greyscale, contrast = 1)
-display(grey_images[[5]]) #visualize contrast/brightness adjustment here
+grey_images <- lapply(img_transposed, greyscale, contrast = 4)
+display(grey_images[[2]]) #visualize contrast/brightness adjustment here
 
-binary<-function(x, adj = 0.5) {
-  binary_img<- x > adj
-  return(binary_img)
-}
-binary_imgs<-lapply(grey_images, binary, adj = 0.32)
-display(binary_imgs[[5]])
+img1<-lapply(grey_images,mapped_avg)
 
-imagesMapped <- lapply(grey_images, mapped, threshold = 0.32) #background intensity threshold adjustment
-
-
-#IMAGE TESTING - do you need to make variable adjustments?
-
-imagesConverted <- image_convert2(imagesMapped[[5]], w = 50, h = 50, offset = 0.001, areathresh = 250, tolerance = 0.8, ext = 4)
-#Change variables above depending on species analyzed
-final_img <- countImages(imagesConverted, normalize = T, removeEdgeCells = T)
-display(final_img)
-
-for (z in 1:length(images)) {
-  imagesConverted <- image_convert2(imagesMapped[[z]], w = 50, h = 50, offset = 0.001, areathresh = 250, tolerance = 0.8, ext = 4)
+for (j in 1:length(images)) {
+  adj1<-median(img1[[j]])
+  adj2<-adj1+0.2
+  imagesMapped <- lapply(grey_images, mapped, threshold = adj2) #background intensity threshold adjustment
+  imagesConverted <- image_convert(imagesMapped[[j]], w = 17, h = 17, offset = 0.001, areathresh = 250, tolerance = 1, ext = 1)
   final_img <- countImages(imagesConverted, normalize = T, removeEdgeCells = T)
   count <- countCells(imagesConverted)
-  Cell.Count[nrow(Cell.Count) + 1, ] <- c(imgNames[[z]], count)
-  analyzed_image <- paste0(sub(".tif", replacement = "", x = imgNames[z]), "_analyzed.tiff")
+  Cell.Count[nrow(Cell.Count) + 1, ] <- c(imgNames[[j]], count)
+  analyzed_image <- paste0(sub(".tif", replacement = "", x = imgNames[j]), "_analyzed.tiff")
   writeImage(final_img, files = paste0(image_savdir, analyzed_image),compression=c("LZW"))
 }
 
@@ -82,6 +75,6 @@ Cell.Count[nrow(Cell.Count) + 1, ] <- c("Imaging Average",cell.av,"null")
 #cell.mL<-(cell.den/2) #change this via total volume
 #Cell.Count[nrow(Cell.Count) + 1, ] <- c("Total Volume Cell per mL",cell.mL,"null")
 
-write.csv(Cell.Count, paste0(savdir, "/Dolichospermum_Med1  counts.csv")) #Change this CSV file name
+write.csv(Cell.Count, paste0(savdir, "/F192_Conc_1E counts.csv")) #Change this CSV file name
 
 beepr::beep(sound=2) #analysis complete
