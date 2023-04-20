14acb87dce6fc4c2dd9a4033264e564cb857d12f
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-04-20 11:35:19 -0400

    Updates: removal of functions within the script, as well as correcting errors

--- analyze_img.R
+++ analyze_img.R
@@ -4,65 +4,6 @@ library(pixmap)
 library(raster)
 library(beepr)
 
-border <- function(x, y) {
-  c(x[1:y[1], 1], x[1:y[1], y[2]], x[1, 1:y[2]], x[y[1], 1:y[2]])
-}
-cell_density <- function(x, FOV = 0.0726, images = 10, filtration.area = 213.8, volume = 1, total.volume = 5) {
-  MCF <- filtration.area / FOV
-  N <- x / images
-  cyano <- (MCF * N) / volume
-  cyano_total <- cyano * total.volume
-  return(cyano_total)
-}
-countCells <- function(x) {
-  countAll <- max(x)
-  dims <- dim(x)
-  border1 <- border(x, dims)
-  ids <- unique(border1[which(border1 != 0)])
-  countInner <- countAll - length(ids)
-  return(countInner)
-}
-countImages <- function(image, normalize = TRUE, removeEdgeCells = TRUE) {
-  if (removeEdgeCells) {
-    dims <- dim(image)
-    border1 <- border(image, dims)
-    ids <- unique(border1[which(border1 != 0)])
-    inner <- rmObjects(image, ids)
-    EBImage::colorLabels(inner, normalize = normalize)
-  } else {
-    EBImage::colorLabels(image, normalize = normalize)
-  }
-}
-greyscale <- function(x, contrast = 2) {
-  x <- contrast * x
-  x <- x[, , 1] + x[, , 2] + x[, , 3]
-  x <- x / max(x)
-  x <- normalize(x, inputRange = c(0.1, 0.75))
-  return(x)
-}
-single_cell_convert <- function(x, w = 17, h = 17, offset = 0.001, areathresh = 50, tolerance= 0.5, ext = 1) {
-  image <- thresh(x, w = w, h = h, offset = offset)
-  image1 <- fillHull(image)
-  image2 <- watershed(distmap(image1), tolerance = tolerance, ext = ext)
-  nf <- computeFeatures.shape(image2)
-  nr <- which(nf[, "s.area"] < areathresh)
-  image3 <- rmObjects(image2, nr)
-  return(image3)
-}
-filamentous_convert <- function(x, w = 17, h = 17, offset = 0.001, areathresh = 50, tolerance= 0.5, ext = 1) {
-  image <- thresh(x, w = w, h = h, offset = offset)
-  image1 <- watershed(distmap(image), tolerance = tolerance, ext = ext)
-  image2 <- fillHull(image1)
-  nf <- computeFeatures.shape(image2)
-  nr <- which(nf[, "s.area"] < areathresh)
-  image3 <- rmObjects(image2, nr)
-  return(image3)
-}
-mapped <- function(x, threshold = 0.3) {
-  x <- as.matrix(x)
-  x[x < threshold] <- 0
-  return(x)
-}
 mapped_avg <- function(x) {
   x <- as.matrix(x)
   return(x)
@@ -75,12 +16,12 @@ if(analysis_type == 'single-cell'){
   grey_images <- lapply(img_transposed, greyscale, contrast = contrast_adj2)
   img1<-lapply(grey_images,mapped_avg)
   for (z in 1:length(images)) {
-    adj1<-median(img1[[j]])
+    adj1<-median(img1[[z]])
     adj2<-adj1+thresh_adj2
     imagesMapped <- lapply(grey_images, mapped, threshold = adj2) #background intensity threshold adjustment
     imagesConverted <- single_cell_convert(imagesMapped[[z]], w = rec_width2, h = rec_height2, offset = 0.001, areathresh = areathresh2, tolerance = tolerance2, ext = ext2)
-    final_img <- countImages(imagesConverted, normalize = T, removeEdgeCells = T)
-    count <- countCells(imagesConverted)
+    final_img <- count_images(imagesConverted, normalize = T, removeEdgeCells = T)
+    count <- count_cells(imagesConverted)
     Cell.Count[nrow(Cell.Count) + 1, ] <- c(imgNames[[z]], count)
     analyzed_image <- paste0(sub(".tif", replacement = "", x = imgNames[z]), "_analyzed.tiff")
     writeImage(final_img, files = paste0(image_file, analyzed_image),compression=c("LZW"))
@@ -102,12 +43,12 @@ if(analysis_type == 'single-cell'){
   grey_images <- lapply(img_transposed, greyscale, contrast = contrast_adj2)
   img1<-lapply(grey_images,mapped_avg)
   for (z in 1:length(images)) {
-    adj1<-median(img1[[j]])
+    adj1<-median(img1[[z]])
     adj2<-adj1+thresh_adj2
     imagesMapped <- lapply(grey_images, mapped, threshold = adj2) #background intensity threshold adjustment
     imagesConverted <- filamentous_convert(imagesMapped[[z]], w = rec_width2, h = rec_height2, offset = 0.001, areathresh = areathresh2, tolerance = tolerance2, ext = ext2)
-    final_img <- countImages(imagesConverted, normalize = T, removeEdgeCells = T)
-    count <- countCells(imagesConverted)
+    final_img <- count_images(imagesConverted, normalize = T, removeEdgeCells = T)
+    count <- count_cells(imagesConverted)
     Cell.Count[nrow(Cell.Count) + 1, ] <- c(imgNames[[z]], count)
     analyzed_image <- paste0(sub(".tif", replacement = "", x = imgNames[z]), "_analyzed.tiff")
     writeImage(final_img, files = paste0(image_file, analyzed_image),compression=c("LZW"))
