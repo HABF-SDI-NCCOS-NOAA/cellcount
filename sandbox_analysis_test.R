67e3ecce03444300052cee248b1d90d0c64f5055
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-05-16 16:15:08 -0400

    Sandbox use - no major changes

--- sandbox_analysis_test.R
+++ sandbox_analysis_test.R
@@ -16,14 +16,14 @@ gc()
 
 Cell.Count <- data.frame(Image_File_Name = character(0), Cell_Count = numeric(0))
 
-file_name<-paste0("F192_") #Change the name of species analyzed here
+file_name<-paste0("E.coli_") #Change the name of species analyzed here
 
 #Change directories here
 savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/CSV_data/")
-image_savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/Convert_Images/Cyano F192_40x/Rep 1/Conc_E/")
-images <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/Cyano F192_40x/Rep 1/Conc_E/"
+image_savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/Convert_Images/E.coli_100x_DAPI/1C/")
+images <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/quantitative_images/E.coli_100x_DAPI/1C/"
                      , pattern = "tif", full.name = T)
-images_names <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/quantitative_images/Cyano F192_40x/Rep 1/Conc_E/"
+images_names <- list.files("C:/Users/Tyler.Harman/Desktop/cellcount_work/cellcount_data/quantitative_images/E.coli_100x_DAPI/1C/"
                            , pattern = "tif", full.name = F)
 
 imgNames <- paste0(file_name, images_names)
@@ -40,7 +40,7 @@ for (j in 1:length(images)) {
   adj1<-median(img1[[j]])
   adj2<-adj1+0.2
   imagesMapped <- lapply(grey_images, mapped, threshold = adj2) #background intensity threshold adjustment
-  imagesConverted <- single_cell_convert(imagesMapped[[j]], w = 17, h = 17, offset = 0.001, areathresh = 250, tolerance = 1, ext = 1)
+  imagesConverted <- single_cell_convert(imagesMapped[[j]], w = 10, h = 10, offset = 0.001, areathresh = 50, tolerance = 0.8, ext = 1)
   final_img <- count_images(imagesConverted, normalize = T, removeEdgeCells = T)
   count <- count_cells(imagesConverted)
   Cell.Count[nrow(Cell.Count) + 1, ] <- c(imgNames[[j]], count)
@@ -75,6 +75,6 @@ Cell.Count[nrow(Cell.Count) + 1, ] <- c("Imaging Average",cell.av,"null")
 #cell.mL<-(cell.den/2) #change this via total volume
 #Cell.Count[nrow(Cell.Count) + 1, ] <- c("Total Volume Cell per mL",cell.mL,"null")
 
-write.csv(Cell.Count, paste0(savdir, "/F192_Conc_1E counts.csv")) #Change this CSV file name
+write.csv(Cell.Count, paste0(savdir, "/E.coli_1C counts.csv")) #Change this CSV file name
 
 beepr::beep(sound=2) #analysis complete
